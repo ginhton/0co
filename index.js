846144bdabebed988801a76ad4db41d81595c3f9
ginhton <siqliin1qyopki@sina.com>
2018-12-08 20:40:57 +0800

    first index.html file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+let fs = require('fs');
+
+let content = "hello wolrd!";
+fs.writeFile('public/index.html', content, function(err) {
+  if (err) { return console.log(err); }
+  console.log("site generated!");
+});
+
+
